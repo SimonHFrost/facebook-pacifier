81a2504288b27f1dcd0cb74eef6fddf80950d294
simonhfrost <simonhfrost@gmail.com>
2016-01-31 18:26:47 +0100

    Add page permission to manifest

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -1,3 +1,3 @@
 (ns main.core)
 (js/alert "Hello world.")
-(.addEventListener (.querySelector js/document "body") "click" (js/alert "click"))
+(.addEventListener (.querySelector js/document "body") "click" (fn [] (js/alert "click")))
